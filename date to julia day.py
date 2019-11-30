92840d27a3a81ba762b67ef32d6f5ef36c5b9fde
zhywwj <56854301+zhywwj@users.noreply.github.com>
2019-11-30 21:52:44 +0800

    Update date to julia day.py

--- date to julia day.py
+++ date to julia day.py
@@ -2,7 +2,7 @@
 from datetime import datetime
 
 def JuliaDay():
-    Date = input ("input date year-month-day: >>")
+    Date = input ("input date year-month-day: >>")   # input example 20190101
     j = datetime.strptime(Date, '%Y%m%d').timetuple()
     print(j.tm_yday)
 
